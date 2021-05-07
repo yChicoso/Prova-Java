1ec398e82a14d5a9d2c617fcaf14ef8e07cfec1d
yChicoso <83352411+yChicoso@users.noreply.github.com>
2021-05-06 21:16:11 -0300

    Add files via upload

---
+++ Filha.java
@@ -0,0 +1,9 @@
+public class Filha {
+
+     String nome;
+
+    public Filha(String nome){
+    this.nome = "Amanda";
+    }
+}
+
---
+++ Pai.java
@@ -0,0 +1,12 @@
+public class Pai extends Filha{
+
+  public static void main(String[] args){
+     
+   public Pai(String nome){
+      super(nome);
+   
+     System.out.println(nome);
+     
+   }
+}
+}
